426d15e79de6aba457f80f85aae6ab53a7232434
SofiDev <angiemeow89@gmail.com>
2024-10-08 16:05:14 -0600

    quick Fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -35,4 +35,20 @@ export default {
 		},
 	  },
 	plugins: [],
+	screens: {
+		'sm': '640px',
+
+
+		'md': '768px',
+		// => @media (min-width: 768px) { ... }
+
+		'lg': '1024px',
+		// => @media (min-width: 1024px) { ... }
+
+		'xl': '1280px',
+		// => @media (min-width: 1280px) { ... }
+
+		'2xl': '1925px',
+		// => @media (min-width: 1925px) { ... }
+	  }
 }
