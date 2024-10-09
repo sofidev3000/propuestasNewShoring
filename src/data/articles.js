84f0c5a76e80aadecb04c406b815f7e8989c50aa
SofiDev <angiemeow89@gmail.com>
2024-10-09 09:28:40 -0600

    responsive adjustments

--- src/data/articles.js
+++ src/data/articles.js
@@ -3,7 +3,7 @@ export const articles = [
         id: 1,
         title: "Nearshoring y electromovilidad impulsan crecimiento de mercado industrial en...",
         excerpt: "Phoenix Capital Group, an unprofitable oil firm selling high-yield debt to retirees is led by a CEO whose past guilty plea for theft they’d rather you not know about.",
-        author: "Global Media",
+        author: "Jhon Doe",
         date: "Octubre 08, 2024",
         featureImage: "https://nxt-global-media.s3.amazonaws.com/near/images/08102024/f6306611f2.webp"
     },
@@ -11,7 +11,7 @@ export const articles = [
         id: 2,
         title: "Industria del mueble y colchones triplicó IED por nearshoring ...",
         excerpt: "La industria mueblera logró triplicar sus inversiones a 63 millones...",
-        author: "Global Media",
+        author: "Jhon Doe",
         date: "Septiembre 26, 2024",
         featureImage: "https://nxt-global-media.s3.amazonaws.com/near/images/30092024/902ed0270d.webp"
     },
@@ -19,7 +19,7 @@ export const articles = [
         id: 3,
         title: "Alemania endurece controles fronterizos para regularizar migración y mejorar seguridad...",
         excerpt: "Alemania implementará controles temporales en fronteras para regularizar la migración,...",
-        author: "Global Media",
+        author: "Jhon Doe",
         date: "Septiembre 20, 2024",
         featureImage: "https://nxt-global-media.s3.amazonaws.com/near/images/10092024/e14b31e102.webp"
     },
@@ -27,7 +27,7 @@ export const articles = [
         id: 4,
         title: "Inglaterra y Gales liberan 1,750 prisioneros para reducir el hacinamiento...",
         excerpt: "El Gobierno laborista de Keir Starmer liberará a 1,750 prisioneros...",
-        author: "Global Media",
+        author: "Jhon Doe",
         date: "Septiembre 20, 2024",
         featureImage: "https://nxt-global-media.s3.amazonaws.com/near/images/10092024/bb1e746118.webp"
     },
@@ -35,7 +35,7 @@ export const articles = [
         id: 5,
         title: "Díaz-Canel y Cuba apoyan a Maduro tras confiscación de avión...",
         excerpt: "Cuba y aliados rechazan la confiscación del avión de Maduro...",
-        author: "Global Media",
+        author: "Jhon Doe",
         date: "Septiembre 20, 2024",
         featureImage: "https://nxt-global-media.s3.amazonaws.com/near/images/04092024/1fa4aab44c.webp"
     },
@@ -43,7 +43,7 @@ export const articles = [
         id: 4,
         title: "Inglaterra y Gales liberan 1,750 prisioneros para reducir el hacinamiento...",
         excerpt: "El Gobierno laborista de Keir Starmer liberará a 1,750 prisioneros...",
-        author: "Global Media",
+        author: "Jhon Doe",
         date: "Septiembre 20, 2024",
         featureImage: "https://nxt-global-media.s3.amazonaws.com/near/images/10092024/bb1e746118.webp"
     },
@@ -51,7 +51,7 @@ export const articles = [
         id: 5,
         title: "Díaz-Canel y Cuba apoyan a Maduro tras confiscación de avión...",
         excerpt: "Cuba y aliados rechazan la confiscación del avión de Maduro...",
-        author: "Global Media",
+        author: "Jhon Doe",
         date: "Septiembre 20, 2024",
         featureImage: "https://nxt-global-media.s3.amazonaws.com/near/images/04092024/1fa4aab44c.webp"
     }
